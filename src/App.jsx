163c4f91b9b0c4e7ada93fa3857a7f49238bcc9a
fernando stubing <fstubing@yahoo.com>
2024-11-05 23:07:37 -0300

    inicio de parcial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Card from "./Components/Card";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card />
+    </div>
+  );
+};
+
+export default App;
