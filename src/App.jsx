7850f387c0fc3f513285b7c87d4dcecaf9720f27
fernando stubing <fstubing@yahoo.com>
2024-11-08 21:54:05 -0300

    subiendo parcial

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,57 @@
+import { useState } from "react";
 import Card from "./Components/Card";
 
 const App = () => {
+  const [user, setUser] = useState({
+    nombre: "",
+    anime: "",
+  });
+  const [show, setShow] = useState(false);
+  const [error, setError] = useState(false);
+  const handleChange = (event) => {
+    setUser({ ...user, [event.target.name]: event.target.value });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (user.nombre.trim().length >= 3 && user.anime.trim().length >= 6) {
+      setShow(true);
+    } else {
+      setError(true);
+    }
+  };
   return (
-    <div>
-      <h1>Carga de estudiantes</h1>
-      <form></form>
-      <Card />
+    <div className="container p-5">
+      <h1 className="text-center">Anime Favorito</h1>
+      {show ? (
+        <Card nombre={user.nombre} anime={user.anime} />
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <label className="form-label">Nombre:</label>
+          <input
+            className="form-control mb-3"
+            placeholder="Ingrese su nombre (no debe ser inferior a tres caracteres)"
+            name="nombre"
+            type="text"
+            value={user.nombre}
+            onChange={handleChange}
+          />
+          <label className="form-label">Anime Favorito:</label>
+          <input
+            className="form-control mb-3"
+            placeholder="Ingrese el nombre de su anime favorito (no debe ser inferior a seis caracteres)"
+            name="anime"
+            type="text"
+            value={user.anime}
+            onChange={handleChange}
+          />
+          <button className="btn btn-primary btn-lg">Enviar</button>
+          {error && (
+            <h4 className="text-danger my-3">
+              Por favor chequea que la información sea correcta
+            </h4>
+          )}
+        </form>
+      )}
     </div>
   );
 };
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,5 +1,13 @@
-const Card = () => {
-  return <div>Card</div>;
+const Card = ({ nombre, anime }) => {
+  return (
+    <div className="card text-bg-success my-5 text-center">
+      <div className="card-header">ENVIO EXITOSO</div>
+      <div className="card-body">
+        <p className="card-text">Hola {nombre}!</p>
+        <p className="card-text">Tu anime favorito es: {anime}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
